0d83bb569f46ae6492a50f1efe9de90825a37c58
eunyoung23 <eun970923@naver.com>
2023-01-29 16:38:56 +0900

    Feat: Telegram 구독, 추천정보 알림 호출 구현

--- src/main/java/com/hermes/presentation/schedule/Scheduler.java
+++ src/main/java/com/hermes/presentation/schedule/Scheduler.java
@@ -1,5 +1,6 @@
 package com.hermes.presentation.schedule;
 
+import com.hermes.application.AlarmRequestService;
 import com.hermes.presentation.dto.feignclient.CrawlingContentsLastUrlDto;
 import com.hermes.application.HermesRequestService;
 import com.hermes.domain.util.ContentsProviderType;
@@ -16,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Scheduler {
     private final HermesRequestService hermesRequestService;
+    private final AlarmRequestService alarmRequestService;
 
     @Scheduled(fixedDelay = 10000)
     public void scheduleFixedRateTask() {
@@ -27,4 +29,15 @@ public class Scheduler {
         hermesRequestService.finaAndInsertYoutubeCrawling(lastTitleList, ContentsProviderType.DREAM_CODING);
         hermesRequestService.findAndInsertJobCrawling(lastTitleList, ContentsProviderType.SARAMIN);
     }
+
+    @Scheduled(fixedDelay = 20000)
+    public void scheduleSubscribeAlarmTask(){
+        alarmRequestService.getSubscribeAlarm();
+    }
+
+    @Scheduled(fixedDelay = 6000000)
+    public void scheduleRecommendAlarmTask(){
+        alarmRequestService.getRecommendAlarm();
+    }
+
 }
